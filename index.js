1a47f7264fd1e2ce867789e45cb9240d5387faee
JonniL111 <kapral089@gmail.com>
2021-03-29 21:15:27 +0300

    add path to routing

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ app.use(express.static(path.join(__dirname, 'front/build')));
 //app.use(express.json());
 
 // An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
+/* app.get('/api/getList', (req,res) => {
   var list = ["item1", "item2", "item3"];
   res.json(list);
   console.log('Sent list of items');
-});
+}); */
+
+app.use(serverRoutes);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/front/build/index.html'));
 });
 
-app.use(serverRoutes);
 
 app.listen(PORT, (err) => {
   if (err) {
