04c5590feca8f7f3e9fcdaed2c4537834fd92352
JonniL111 <kapral089@gmail.com>
2021-03-29 14:36:35 +0300

    1

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 // An api endpoint that returns a short list of items
-/* app.get('/api/getList', (req,res) => {
+app.get('/api/getList', (req,res) => {
   var list = ["item1", "item2", "item3"];
   res.json(list);
   console.log('Sent list of items');
@@ -23,7 +23,7 @@ app.use(express.json());
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/front/build/index.html'));
-}); */
+});
 
 app.use(serverRoutes);
 
